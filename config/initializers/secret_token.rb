bc6ec7c527ee1312e1819597d0dcebc94acc4494
heartsentwined <heartsentwined@cogito-lab.com>
2013-04-01 22:03:34 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmblemTest::Application.config.secret_token = 'ba1d3e9d14d403713ffab0d0c51f9b461815518bd4ef5cd13504dec4000db95582c06e3bac7669ee8c8c6a3ad3c813a9c7c7ed58251681bbb6287af1c8b71d6c'
